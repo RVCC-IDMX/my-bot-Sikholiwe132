2db9b00c4f3f604935af88bcf006992b32a73752
Sikholiwe132 <genesissibisi@gmail.com>
2023-12-20 05:52:00 -0500

    update changes

--- commands/utility/cowsay.js
+++ commands/utility/cowsay.js
@@ -4,7 +4,7 @@ const cowsay = require('cowsay');
 
 const cowList = [
     'ackbar', 'aperture-blank', 'aperture', 'armadillo', 'atat', 'atom', 'awesome-face', 'banana', 'bearface', 'beavis.zen', 'bees', 'bill-the-cat', 'biohazard', 'bishop', 'black-mesa',
-    'bong', 'box', 'broken-heart', 'bud-frogs', 'bunny', 'C3PO', 'cake-with-candles', 'cake', 'cat', 'cat2', 'catfence', 'charizardvice', 'charlie', 'cheese', 'chessmen', 'chito', 'claw-arm',
+    'bong', 'box', 'broken-heart', 'bud-frogs', 'bunny', 'cow', 'C3PO', 'cake-with-candles', 'cake', 'cat', 'cat2', 'catfence', 'charizardvice', 'charlie', 'cheese', 'chessmen', 'chito', 'claw-arm',
     'clippy', 'companion-cube', 'cower', 'cowfee', 'cthulhu-mini', 'cube', 'daemon', 'dalek-shooting', 'dalek', 'default', 'docker-whale', 'doge', 'dolphin', 'dragon-and-cow', 'dragon', 'ebi_furai',
     'elephant-in-snake', 'elephant', 'elephant2', 'explosion', 'eyes', 'fat-banana', 'fat-cow', 'fence', 'fire', 'flaming-sheep', 'fox', 'ghost', 'ghostbusters', 'glados', 'goat', 'goat2', 'golden-eagle',
     'hand', 'happy-whale', 'hedgehog', 'hellokitty', 'hippie', 'hiya', 'hiyoko', 'homer', 'hypno', 'ibm', 'iwashi', 'jellyfish', 'karl_marx', 'kilroy', 'king', 'kiss', 'kitten', 'kitty', 'knight', 'koala',
@@ -29,23 +29,36 @@ module.exports = {
             .setDescription('Which cow says it')
             .setRequired(true)),
     async execute(interaction) {
-        const cowText = interaction.options.getString('text');
-        const cowChoice = interaction.options.getString('cow');
+        try {
+            const cowText = interaction.options.getString('text');
+            const cowChoice = interaction.options.getString('cow');
 
-        if (!cowList.includes(cowChoice)) {
-            await interaction.reply('Cow choice not recognized! Try again');
-            return;
-        }
+            console.log('Received cowText:', cowText);
+            console.log('Received cowChoice:', cowChoice);
 
-        const cowMessage = cowsay.say({
-            text: cowText,
-            f: cowChoice,
-        });
+            if (!cowList.includes(cowChoice)) {
+                console.log('Invalid cow choice:', cowChoice);
+                await interaction.reply('Cow choice not recognized! Try again');
+                return;
+            }
 
-        if (cowMessage.length <= 2000) {
-            await interaction.reply(`\`\`\`${cowMessage}\`\`\``);
-        } else {
-            await interaction.reply('Cow exceeds limit! Please try again!');
+            const cowMessage = cowsay.say({
+                text: cowText,
+                f: cowChoice,
+            });
+
+            console.log('Generated cowMessage:', cowMessage);
+
+            if (cowMessage.length <= 2000) {
+                await interaction.reply(`\`\`\`${cowMessage}\`\`\``);
+                console.log('Successfully replied with cowMessage.');
+            } else {
+                console.log('Cow exceeds limit:', cowMessage.length);
+                await interaction.reply('Cow exceeds limit! Please try again!');
+            }
+        } catch (error) {
+            console.error('An error occurred:', error);
+            await interaction.reply('An error occurred. Please try again later.');
         }
     },
 };
